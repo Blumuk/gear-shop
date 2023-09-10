17fdffe34c0a62e965c580a685e1ee5e69898dfc
Blumuk <corbin.blumer@yahoo.com>
2023-09-10 14:12:01 +0100

    transfer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,16 @@
 
 
 
-<main>
-    <div class="w-full h-16 bg-slate-600 flex flex-row items-center">
+<main class="">
+    <div class="w-full h-16 bg-slate-700 flex flex-row items-center shadow-md">
         <img class="w-12 h-12 ml-2" src={bt} alt="">
-        <h1 class="font-sans font-semibold text-xl ml-2 text-white">Gear Shop</h1>
-        <input class="w-96 ml-auto h-7" type="text">
-        <button class="h-7 w-7 bg-black mr-auto text-white">S</button>
-        <div class="min-w-28 h-full bg-lime-200 flex flex-row items-center">
+        <h1 class="font-sans font-semibold text-xl ml-2 text-white">Fleagear</h1>
+        <input placeholder="Search for Gear..." class="w-96 ml-auto h-7 outline-none rounded-l-md shadow-md px-2" type="text">
+        <button class="h-7 w-7 bg-slate-800 mr-auto text-white rounded-r-md shadow-md"></button>
+        <div class="min-w-28 h-full flex flex-row items-center">
             <img class="w-11 h-11 ml-3 mr-3 rounded-full" src="https://machohairstyles.com/wp-content/uploads/2017/03/haircut-for-men-with-square-faces-9.jpg" alt="">
-            <h6 class="mr-3">Username</h6>
-
+            <h6 class="font-sans font-semibold text-l mr-3 text-white">Username</h6>
         </div>
-
-        
     </div>
-    <slot></slot>
+    <slot class="h-full"></slot>
 </main>
